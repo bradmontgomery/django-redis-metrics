c84dddc060eaf98c74900fd6c77dc449d3007745
Brad Montgomery <brad@bradmontgomery.net>
2012-12-27 17:10:15 -0600

    added a delete_test_metrics util function

--- redis_metrics/utils.py
+++ redis_metrics/utils.py
@@ -1,3 +1,4 @@
+import datetime
 from .models import R
 
 
@@ -14,15 +15,16 @@ def gague(slug, current_value):
     _r.gague(slug, current_value)
 
 
+def _dates(num):
+    today = datetime.date.today()
+    return (today - datetime.timedelta(days=d) for d in range(num))
+
+
 def generate_test_metrics(slug='test-metric', num=100):
     """Generate some dummy (but increasing) metrics for the given ``slug`` and
     the past ``num`` days."""
-    import datetime
-    today = datetime.date.today()
-    dates = (today - datetime.timedelta(days=d) for d in range(num))
-
     i = 1
-    for date in dates:
+    for date in _dates(num):
         for key in _r._build_keys(slug, date=date):
             # The following is normally done in _r.metric, but we're adding
             # "old" metrics here, so this is duplicate code.
@@ -30,3 +32,11 @@ def generate_test_metrics(slug='test-metric', num=100):
             for x in range(i):
                 _r.r.incr(key)  # incr the key an increasing number of times
         i += 1
+
+
+def delete_test_metrics(slug='test-metric', num=100):
+    """Deletes the metrics created by ``generate_test_metrics``."""
+    for date in _dates(num):
+        keys = _r._build_keys(slug, date=date)
+        _r.r.srem(_r._metric_slugs_key, *keys)  # remove metric slugs
+        _r.r.delete(*keys)  # delete the metrics
