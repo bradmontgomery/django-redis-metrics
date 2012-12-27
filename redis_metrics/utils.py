826926d19ccdacbe10c22bae1104940c51aed38e
Brad Montgomery <brad@bradmontgomery.net>
2012-12-27 16:59:29 -0600

    added a util function to generate test metrics

--- redis_metrics/utils.py
+++ redis_metrics/utils.py
@@ -12,3 +12,21 @@ def metric(slug, num=1, **kwargs):
 def gague(slug, current_value):
     """Set a value for a Guage"""
     _r.gague(slug, current_value)
+
+
+def generate_test_metrics(slug='test-metric', num=100):
+    """Generate some dummy (but increasing) metrics for the given ``slug`` and
+    the past ``num`` days."""
+    import datetime
+    today = datetime.date.today()
+    dates = (today - datetime.timedelta(days=d) for d in range(num))
+
+    i = 1
+    for date in dates:
+        for key in _r._build_keys(slug, date=date):
+            # The following is normally done in _r.metric, but we're adding
+            # "old" metrics here, so this is duplicate code.
+            _r.r.sadd(_r._metric_slugs_key, key)  # keep track of the keys
+            for x in range(i):
+                _r.r.incr(key)  # incr the key an increasing number of times
+        i += 1
