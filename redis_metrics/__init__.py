36d4f6de7e298a672cc0ba846a20d71e0cbbb2af
Brad Montgomery <brad@bradmontgomery.net>
2012-12-30 15:28:59 -0600

    :confounded:

--- redis_metrics/__init__.py
+++ redis_metrics/__init__.py
@@ -1,3 +1,10 @@
-from .utils import gauge, metric
-
 VERSION = (0, 1, 0)
+
+# Sigh. I want to be able to do this:
+#   >>> from redis_metrics import metric
+# BUT, that breaks setup (because setup reads the VERSION here), and
+# the ``redis`` dependency hasn't yet been installed.
+try:
+    from .utils import gauge, metric
+except ImportError:
+    pass
