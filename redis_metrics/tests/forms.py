5e7ad5a4d55bf0bb3429861faefb5044dacad49d
Brad Montgomery <brad@bradmontgomery.net>
2013-05-13 23:06:11 -0400

    complete tests for TestAggregateMetricForm

--- redis_metrics/tests/forms.py
+++ redis_metrics/tests/forms.py
@@ -1,5 +1,4 @@
-#from mock import call, patch, Mock
-
+from mock import call, patch
 from django.test import TestCase
 
 from ..forms import AggregateMetricForm, MetricCategoryForm
@@ -7,16 +6,29 @@ from ..forms import AggregateMetricForm, MetricCategoryForm
 
 class TestAggregateMetricForm(TestCase):
 
-    def setUp(self):
-        self.form = AggregateMetricForm()
-
     def test_form(self):
-        assert False
-        # Test that form has choices populated from R.metric_slugs
+        """Test that form has choices populated from R.metric_slugs"""
+        # Set up a mock result for R.metric_slugs
+        config = {'return_value.metric_slugs.return_value': ['test-slug']}
+        with patch('redis_metrics.forms.R', **config) as mock_R:
+            form = AggregateMetricForm()
+            mock_R.assert_has_calls([
+                call(),
+                call().metric_slugs(),
+            ])
+            self.assertEqual(
+                form.fields['metrics'].choices,
+                [('test-slug', 'test-slug')]
+            )
 
-    def test_clean(self):
-        assert False
-        # Verify we get expected results from cleaned_data
+    def test_cleaned_data(self):
+        """Verify we get expected results from cleaned_data"""
+        # Set up a mock result for R.metric_slugs
+        config = {'return_value.metric_slugs.return_value': ['test-slug']}
+        with patch('redis_metrics.forms.R', **config):
+            form = AggregateMetricForm({"metrics": ["test-slug"]})
+            self.assertTrue(form.is_valid())
+            self.assertEqual(form.cleaned_data, {"metrics": ["test-slug"]})
 
 
 class TestMetricCategoryForm(TestCase):
