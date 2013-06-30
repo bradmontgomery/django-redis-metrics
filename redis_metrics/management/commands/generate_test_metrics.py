d871c4a28a30317797e69ca000f1c784c237f05f
Brad Montgomery <brad@bradmontgomery.net>
2013-06-30 08:51:05 -0500

    you can now provide metric names for test metrics

--- redis_metrics/management/commands/generate_test_metrics.py
+++ redis_metrics/management/commands/generate_test_metrics.py
@@ -1,19 +1,17 @@
-from datetime import date
-from django.core.management.base import NoArgsCommand
+from django.core.management.base import BaseCommand, CommandError
 from redis_metrics.utils import generate_test_metrics
 
 
-class Command(NoArgsCommand):
+class Command(BaseCommand):
+    args = '<metric-name> [<metric-name> ...]'
     help = "Creates Lots of Dummy Metrics"
     can_import_settings = True
 
-    def handle_noargs(self, **options):
-        metric_names = [
-            'test-thingy',
-            'test-flangey',
-            'test-foobley',
-            'test-blahblah',
-        ]
+    def handle(self, *args, **options):
+        if len(args) == 0:
+            raise CommandError("You must provide at least one metric name")
+        metric_names = args
+
         days = 365 * 3  # 3 years of data
         for m in metric_names:
             generate_test_metrics(m, num=days, randomize=True)
