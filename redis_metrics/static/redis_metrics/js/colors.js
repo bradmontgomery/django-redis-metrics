6c66604fefdc5ac852291d613283193965e3abb7
Brad Montgomery <brad@bradmontgomery.net>
2014-07-05 14:29:12 -0500

    move yellow to the back of the list of colors, and darken it a bit because it's difficul to see.

--- redis_metrics/static/redis_metrics/js/colors.js
+++ redis_metrics/static/redis_metrics/js/colors.js
@@ -15,23 +15,23 @@ var Color = function(index) {
     var colors = [
         "255,102,102", // Red
         "255,178,102", // Orange
-        "255,255,102", // Yellow
         "102,255,102", // Green
-        "102,255,255", // Turquoise
         "102,102,255", // Blue
+        "102,255,255", // Turquoise
         "178,102,255", // Purple
         "255,102,178", // Pink
+        "204,204,0",   // Yellow
     ];
     // Highlight Colors
     var hl_colors = [
         "255,0,0",   // Red
         "255,128,0", // Orange,
-        "255,255,0", // Yellow
         "0,255,0",   // Green
-        "0,255,255", // Turquoise
         "0,0,255",   // Blue
+        "0,255,255", // Turquoise
         "127,0,255", // Purple
         "255,0,127", // Pink
+        "255,255,0", // Yellow
     ];
 
     return {
