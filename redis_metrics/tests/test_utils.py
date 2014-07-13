7de8dcf79fc8161f1fe095699ae4f941768803ca
Brad Montgomery <brad@bradmontgomery.net>
2014-07-13 15:54:29 -0500

    updated generate_test_metrics so it only creates metrics for daily and greater granularities; fixed a bug so it stores the slug as a metric key instead of all keys; removed the _dates function since it's no longer used anywhere; updated tests

--- redis_metrics/tests/test_utils.py
+++ redis_metrics/tests/test_utils.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 from mock import call, patch, Mock
 
 from django.conf import settings
@@ -86,20 +86,20 @@ class TestUtils(TestCase):
                 call().gauge("test-slug", 9000),
             ])
 
-    def test__dates(self):
-        # The following line is the expected result of _dates for 5 days
-        d = datetime(2014, 1, 1, 2, 30, 45)
-        with patch('redis_metrics.utils.datetime') as mock_datetime:
-            mock_datetime.utcnow.return_value = d
-            expected = (d - timedelta(days=i) for i in range(5))
-            result = utils._dates(5)
-            self.assertEqual(type(expected), type(result))
-            self.assertEqual(list(expected), list(result))
-
     def test_generate_test_metrics(self):
+        keys = [
+            'm:test-slug:s:2000-01-02-03-04-05',
+            'm:test-slug:i:2000-01-02-03-04',
+            'm:test-slug:h:2000-01-02-03',
+            'm:test-slug:2000-01-02',
+            'm:test-slug:w:2000-01',
+            'm:test-slug:m:2000-01',
+            'm:test-slug:y:2000',
+        ]
         config = {
-            '_build_keys.return_value': ['key'],
+            '_build_keys.return_value': keys,
             '_metric_slugs_key': 'MSK',
+            '_date_range.return_value': [datetime.utcnow()],
         }
         mock_r = Mock(**config)
         config = {'return_value': mock_r}
@@ -108,30 +108,52 @@ class TestUtils(TestCase):
             with patch("redis_metrics.utils.random") as mock_random:
                 mock_random.randint.return_value = 9999
                 utils.generate_test_metrics(
-                    slug="test_slug", num=1, randomize=True
+                    slug="test-slug", num=1, randomize=True, increment_value=1
                 )
                 mock_get_r.assert_called_once_with()
-                mock_r.r.sadd.assert_called_once_with('MSK', 'key')
+                mock_r.r.sadd.assert_called_once_with('MSK', 'test-slug')
                 mock_random.seed.assert_called_once_with()
-                mock_random.randint.assert_called_once_with(0, 100 + 100)
-                mock_r.r.incr.assert_called_once_with('key', 9999)
+                mock_random.randint.assert_has_calls([
+                    call(0, 1), call(0, 1), call(0, 1), call(0, 1),
+                ])
+                mock_r.r.incr.assert_has_calls([
+                    call('m:test-slug:2000-01-02', 9999),
+                    call('m:test-slug:w:2000-01', 9999),
+                    call('m:test-slug:m:2000-01', 9999),
+                    call('m:test-slug:y:2000', 9999),
+                ])
 
             mock_get_r.reset_mock()
             mock_r.reset_mock()
 
             # When called with random = False
             utils.generate_test_metrics(
-                slug="test_slug", num=1, randomize=False
+                slug="test-slug", num=1, randomize=False, increment_value=1
             )
             mock_get_r.assert_called_once_with()
-            mock_r.r.sadd.assert_called_once_with('MSK', 'key')
-            mock_r.r.incr.assert_called_once_with('key', 100)
+            mock_r.r.sadd.assert_called_once_with('MSK', 'test-slug')
+            mock_r.r.incr.assert_has_calls([
+                call('m:test-slug:2000-01-02', 0),
+                call('m:test-slug:w:2000-01', 0),
+                call('m:test-slug:m:2000-01', 0),
+                call('m:test-slug:y:2000', 0),
+            ])
 
     def test_generate_test_metrics_with_cap(self):
+        keys = [
+            'm:test-slug:s:2000-01-02-03-04-05',
+            'm:test-slug:i:2000-01-02-03-04',
+            'm:test-slug:h:2000-01-02-03',
+            'm:test-slug:2000-01-02',
+            'm:test-slug:w:2000-01',
+            'm:test-slug:m:2000-01',
+            'm:test-slug:y:2000',
+        ]
         config = {
-            '_build_keys.return_value': ['key'],
+            '_build_keys.return_value': keys,
             '_metric_slugs_key': 'MSK',
             'r.get.return_value': 100,
+            '_date_range.return_value': [datetime.utcnow()],
         }
         mock_r = Mock(**config)
         config = {'return_value': mock_r}
@@ -140,28 +162,34 @@ class TestUtils(TestCase):
             with patch("redis_metrics.utils.random") as mock_random:
                 mock_random.randint.return_value = 9999
                 utils.generate_test_metrics(
-                    slug="test_slug", num=1, randomize=True, cap=5
+                    slug="test-slug", num=1, randomize=True, cap=5, increment_value=1
                 )
                 mock_get_r.assert_called_once_with()
-                mock_r.r.sadd.assert_called_once_with('MSK', 'key')
+                mock_r.r.sadd.assert_called_once_with('MSK', 'test-slug')
                 mock_random.seed.assert_called_once_with()
-                mock_random.randint.assert_called_once_with(0, 100 + 100)
+                mock_random.randint.assert_has_calls([
+                    call(0, 1), call(0, 1), call(0, 1), call(0, 1),
+                ])
                 # Should be no increment due to cap
-                mock_r.r.incr.assert_called_once_with('key', 0)
+                mock_r.r.incr.assert_has_calls([
+                    call('m:test-slug:2000-01-02', 0),
+                    call('m:test-slug:w:2000-01', 0),
+                    call('m:test-slug:m:2000-01', 0),
+                    call('m:test-slug:y:2000', 0),
+                ])
 
             mock_get_r.reset_mock()
             mock_r.reset_mock()
 
     def test_delete_test_metrics(self):
         d = datetime.utcnow()
-        with patch('redis_metrics.utils._dates') as mock_dates:
-            mock_dates.return_value = [d]
-            with patch('redis_metrics.utils.get_r') as mock_get_r:
-                mock_r = mock_get_r.return_value
-                mock_r._metric_slugs_key = 'MSK'
-                mock_r._build_keys.return_value = ['keys']
+        with patch('redis_metrics.utils.get_r') as mock_get_r:
+            mock_r = mock_get_r.return_value
+            mock_r._metric_slugs_key = 'MSK'
+            mock_r._build_keys.return_value = ['keys']
+            mock_r._date_range.return_value = [d]
 
-                utils.delete_test_metrics(slug="test-metric", num=1)
-                mock_r._build_keys.assert_called_once_with("test-metric", date=d)
-                mock_r.r.srem.assert_called_once_with('MSK', 'keys')
-                mock_r.r.delete.assert_called_once_with('keys')
+            utils.delete_test_metrics(slug="test-metric", num=1)
+            mock_r._build_keys.assert_called_once_with("test-metric", date=d)
+            mock_r.r.srem.assert_called_once_with('MSK', 'test-metric')
+            mock_r.r.delete.assert_called_once_with('keys')
--- redis_metrics/utils.py
+++ redis_metrics/utils.py
@@ -24,45 +24,47 @@ def gauge(slug, current_value):
     get_r().gauge(slug, current_value)
 
 
-def _dates(num):
-    """Yields a generator of datetime objects for the past ``num`` days"""
-    now = datetime.utcnow()
-    return (now - timedelta(days=d) for d in range(num))
-
-
-def generate_test_metrics(slug='test-metric', num=100, randomize=False, cap=None):
+def generate_test_metrics(slug='test-metric', num=100, randomize=False,
+                          cap=None, increment_value=100):
     """Generate some dummy metrics for the given ``slug``.
 
     * ``slug`` -- the Metric slug
     * ``num`` -- Number of days worth of metrics (default is 100)
     * ``randomize`` -- Generate random metric values (default is False)
     * ``cap`` -- If given, cap the maximum metric value.
+    * ``increment_value`` -- The amount by which we increment metrics on
+      subsequent days. If ``randomize`` is True, this value is used to
+      generate a ceiling for random values.
+
+    NOTE: This only generates metrics for daily and larger granularities.
 
     """
-    _r = get_r()
-    i = 100
+    r = get_r()
+    i = 0
     if randomize:
         random.seed()
 
-    for date in _dates(num):
-        for key in _r._build_keys(slug, date=date):
-            # The following is normally done in _r.metric, but we're adding
+    r.r.sadd(r._metric_slugs_key, slug)  # Store the slug created.
+    for date in r._date_range('daily', datetime.utcnow() - timedelta(days=num)):
+        # Only keep the keys for daily and above granularities.
+        keys = r._build_keys(slug, date=date)
+        keys = [k for k in keys if k.split(":")[2] not in ['i', 's', 'h']]
+        for key in keys:
+            # The following is normally done in r.metric, but we're adding
             # metrics for past days here, so this is duplicate code.
-            _r.r.sadd(_r._metric_slugs_key, key)  # keep track of the keys
             value = i
             if randomize:
-                value = random.randint(0, i + 100)
-            if cap and _r.r.get(key) >= cap:
+                value = random.randint(0, i + increment_value)
+            if cap and r.r.get(key) >= cap:
                 value = 0  # Dont' increment this one any more.
-            _r.r.incr(key, value)
-
-        i += 100
+            r.r.incr(key, value)
+        i += increment_value
 
 
 def delete_test_metrics(slug='test-metric', num=100):
     """Deletes the metrics created by ``generate_test_metrics``."""
-    _r = get_r()
-    for date in _dates(num):
-        keys = _r._build_keys(slug, date=date)
-        _r.r.srem(_r._metric_slugs_key, *keys)  # remove metric slugs
-        _r.r.delete(*keys)  # delete the metrics
+    r = get_r()
+    for date in r._date_range('daily', datetime.utcnow() - timedelta(days=num)):
+        keys = r._build_keys(slug, date=date)
+        r.r.srem(r._metric_slugs_key, slug)  # remove metric slugs
+        r.r.delete(*keys)  # delete the metrics
