6104382dc0fbcf7f041e5e1594db9a36a02d4b40
Brad Montgomery <brad@bradmontgomery.net>
2015-11-18 14:39:18 -0600

    adding a proper AppConfig

--- redis_metrics/__init__.py
+++ redis_metrics/__init__.py
@@ -4,3 +4,5 @@ try:
     from .utils import gauge, metric, set_metric  # NOQA
 except ImportError:  # pragma: no cover
     pass  # pragma: no cover
+
+default_app_config = 'redis_metrics.apps.RedisMetricsConfig'
